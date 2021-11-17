c69bd1d1cf12377ff8efe47474c494740d42476a
id3s3c <51968606+id3s3c@users.noreply.github.com>
2021-11-17 20:47:45 -0300

    removing analytics

--- index.py
+++ index.py
@@ -4,10 +4,7 @@ import mss
 import pyautogui
 import time
 import sys
-
 import yaml
-
-import requests
 
 cat = """
                                                 _
@@ -36,24 +33,6 @@ cat = """
 
 print(cat)
 
-headers = {
-    'authority': 'plausible.io',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-    'content-type': 'text/plain',
-    'accept': '*/*',
-    'sec-gpc': '1',
-    'origin': 'https://mpcabete.xyz',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://mpcabete.xyz/',
-    'accept-language': 'en-US,en;q=0.9',
-}
-
-data = '{"n":"pageview","u":"https://mpcabete.xyz/bombcrypto/","d":"mpcabete.xyz","r":"https://mpcabete.xyz/","w":1182}'
-
-response = requests.post('https://plausible.io/api/event', headers=headers, data=data)
-
 if __name__ == '__main__':
 
     stream = open("config.yaml", 'r')
@@ -65,12 +44,6 @@ pyautogui.PAUSE = c['time_intervals']['interval_between_moviments']
 pyautogui.FAILSAFE = True
 hero_clicks = 0
 login_attempts = 0
-
-
-
-
-
-
 
 go_work_img = cv2.imread('targets/go-work.png')
 commom_img = cv2.imread('targets/commom-text.png')
@@ -119,7 +92,6 @@ def printSreen():
         # The screen part to capture
         monitor = {"top": 160, "left": 160, "width": 1000, "height": 135}
 
-        # Grab the data
         #sct_img = np.array(sct.grab(monitor))
         sct_img = np.array(sct.grab(sct.monitors[0]))
         return sct_img[:,:,:3]
